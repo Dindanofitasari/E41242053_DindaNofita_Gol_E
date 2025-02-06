718bc33c220c91d66c56da1e84e62a4283040c99
Dindanofitasari <sarinofitadinda@gmail.com>
2025-02-06 14:42:40 +0700

    M1_P1

---
+++ SistemInformasi_M1P1.java
@@ -0,0 +1,7 @@
+package sisteminformasi_m1p1;
+public class SistemInformasi_M1P1 {
+    public static void main(String[] args) {
+        System.out.println("Nama\t: Dinda Nofitasari \nNIM\t: E41242053 \nProdi\t: Teknik Informatika");
+    }
+    
+}
